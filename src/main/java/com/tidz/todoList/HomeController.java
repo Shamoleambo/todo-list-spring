03147cd8c8f27463d3eb5e4c1ccb493ad8c508b9
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-02 11:01:47 -0300

    create HomeController to display the thymeleaf page

---
+++ src/main/java/com/tidz/todoList/HomeController.java
@@ -0,0 +1,13 @@
+package com.tidz.todoList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String viewHomePage() {
+		return "todos";
+	}
+}
