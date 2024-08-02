0ffa5d1c4a1eff5e2879d27435ebb1640c334db3
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-02 09:22:22 -0300

    create createTodo() method

--- src/main/java/com/tidz/todoList/TodoController.java
+++ src/main/java/com/tidz/todoList/TodoController.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,5 +24,10 @@ public class TodoController {
 	@GetMapping("/{id}")
 	public Optional<Todo> getTodoById(@PathVariable Long id) {
 		return this.todoService.getTodoById(id);
+	}
+
+	@PostMapping
+	public Todo createTodo(@RequestBody Todo todo) {
+		return this.todoService.saveTodo(todo);
 	}
 }
