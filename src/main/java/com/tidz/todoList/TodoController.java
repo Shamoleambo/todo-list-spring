a15d431014c5d250e2a64b9695bf8ecaff512d01
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-02 09:30:27 -0300

    create deleteTodo() method

--- src/main/java/com/tidz/todoList/TodoController.java
+++ src/main/java/com/tidz/todoList/TodoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,5 +39,10 @@ public class TodoController {
 		todo.setTitle(todoDetails.getTitle());
 		todo.setCompleted(todoDetails.isCompleted());
 		return todoService.saveTodo(todo);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteTodo(@PathVariable Long id) {
+		this.todoService.deleteTodoById(id);
 	}
 }
