ee60cbdab26dd79ce8911fbfb351fb84de772855
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-02 16:36:42 -0300

    refactor HomeController class

--- src/main/java/com/tidz/todoList/controller/HomeController.java
+++ src/main/java/com/tidz/todoList/controller/HomeController.java
@@ -1,13 +1,26 @@
 package com.tidz.todoList.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.tidz.todoList.service.TodoService;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private TodoService todoService;
+
 	@GetMapping("/")
-	public String viewHomePage() {
+	public String viewHomePage(@AuthenticationPrincipal UserDetails userDetails) {
 		return "todos";
+	}
+
+	@GetMapping("/user")
+	public String userInfo(@AuthenticationPrincipal UserDetails userDetails) {
+		return "Username: " + userDetails.getUsername();
 	}
 }
