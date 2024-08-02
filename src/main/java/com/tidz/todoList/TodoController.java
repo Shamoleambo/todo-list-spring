73ef921082a9d9b1f5de9eca60c087a2d5f080d4
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-02 09:18:33 -0300

    create TodoController

---
+++ src/main/java/com/tidz/todoList/TodoController.java
@@ -0,0 +1,12 @@
+package com.tidz.todoList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+
+	@Autowired
+	private TodoService todoService;
+
+}
