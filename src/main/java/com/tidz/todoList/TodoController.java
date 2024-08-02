31d18b434f55278a1cf7d11659b88b0d9e99f5ab
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-02 09:21:10 -0300

    create getTodoById() method

--- src/main/java/com/tidz/todoList/TodoController.java
+++ src/main/java/com/tidz/todoList/TodoController.java
@@ -1,8 +1,11 @@
 package com.tidz.todoList;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +14,13 @@ public class TodoController {
 	@Autowired
 	private TodoService todoService;
 
+	@GetMapping
 	public List<Todo> getAllTodos() {
 		return this.todoService.getAllTodos();
+	}
+
+	@GetMapping("/{id}")
+	public Optional<Todo> getTodoById(@PathVariable Long id) {
+		return this.todoService.getTodoById(id);
 	}
 }
