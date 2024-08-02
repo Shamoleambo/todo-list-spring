d5d8d46de7595cde188f2846843860fd6f58116f
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-02 11:32:57 -0300

    create CustomDetailsService class

---
+++ src/main/java/com/tidz/todoList/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.tidz.todoList.service;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.tidz.todoList.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<com.tidz.todoList.entity.User> userOptional = this.userRepo.findByUserName(username);
+		com.tidz.todoList.entity.User user = userOptional
+				.orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+		return new User(user.getUsername(), user.getPassword(),
+				user.getRoles().stream().map(role -> new SimpleGrantedAuthority(role)).collect(Collectors.toList()));
+	}
+
+}
