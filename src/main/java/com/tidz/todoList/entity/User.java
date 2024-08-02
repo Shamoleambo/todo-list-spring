4da94a7ffeff1cff77576a3996309151621132fa
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-02 11:16:24 -0300

    create User entity

---
+++ src/main/java/com/tidz/todoList/entity/User.java
@@ -0,0 +1,65 @@
+package com.tidz.todoList.entity;
+
+import java.util.Collection;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String username;
+	private String password;
+	@ElementCollection(fetch = FetchType.EAGER)
+	private Collection<String> roles;
+
+	public User() {
+
+	}
+
+	public User(String username, String password, Collection<String> roles) {
+		this.username = username;
+		this.password = password;
+		this.roles = roles;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Collection<String> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Collection<String> roles) {
+		this.roles = roles;
+	}
+
+}
